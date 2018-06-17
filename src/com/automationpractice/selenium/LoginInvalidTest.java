187582e9609b201a2016e085c2003ff272eb3e6e
Nur Alfi Ekowati <nuralfiekowati@gmail.com>
2018-06-17 22:26:22 +0700

    +Add login files
    Adding files to test the login validation.

--- src/com/automationpractice/selenium/LoginInvalidTest.java
+++ src/com/automationpractice/selenium/LoginInvalidTest.java
@@ -6,28 +6,24 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-class LoginValidationTest {
-
-	WebElement element;
-
-	public void validLogin(WebDriver driver) {
-
+class LoginInvalidTest {
+	
+WebElement element;
+	
+	public void invalidLogin(WebDriver driver) {
+		
 		element = driver.findElement(By.className("login"));
 		element.click();
-
-		driver.findElement(By.id("email")).sendKeys("nuralfiekowati@yahoo.com");
-		driver.findElement(By.id("passwd")).sendKeys("lalala");
+		
+		driver.findElement(By.id("email")).sendKeys("abcde@yahoo.com");
+		driver.findElement(By.id("passwd")).sendKeys("abcdef12");
 		driver.findElement(By.id("SubmitLogin")).click();
-
+		
 		List<WebElement> errorList = driver.findElements(By.id("authentication"));
-
+		
 		System.out.println("Error count: " + errorList.size());
-
-		if (errorList != null && errorList.size() > 0) {
-
-			System.out.println("Authentication failed.!");
-		}
-
+		
 	}
+
 
 }
