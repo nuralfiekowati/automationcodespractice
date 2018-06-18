2be0dd2ee14243fdcf6898bd94c2d5d4f71920ea
Nur Alfi Ekowati <nuralfiekowati@gmail.com>
2018-06-18 16:48:14 +0700

    -Update file
    Updating file to test invalid login.

--- src/com/automationpractice/selenium/update/LoginInvalidTest.java
+++ src/com/automationpractice/selenium/update/LoginInvalidTest.java
@@ -7,27 +7,24 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 class LoginInvalidTest {
-	
-WebElement element;
-	
+
+	WebElement element;
+
 	public void invalidLogin(WebDriver driver) {
-		
+
 		element = driver.findElement(By.className("login"));
 		element.click();
-		
+
 		driver.findElement(By.id("email")).sendKeys("abcde@yahoo.com");
 		driver.findElement(By.id("passwd")).sendKeys("abcdef12");
 		driver.findElement(By.id("SubmitLogin")).click();
-		
-//		List<WebElement> errorList = driver.findElements(By.id("authentication"));
+
 		List<WebElement> errorList = driver.findElements(By.className("page-heading"));
-		
-		if (errorList != null && errorList.size() > 0)
-		{
+
+		if (errorList != null && errorList.size() > 0) {
 			System.out.println("Error found: Authentication failed!");
 		}
-		
-	}
 
+	}
 
 }
