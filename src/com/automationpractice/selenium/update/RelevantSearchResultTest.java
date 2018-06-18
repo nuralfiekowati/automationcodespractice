81e8d7adab499f7be68c062dbf95a16246e4e695
Nur Alfi Ekowati <nuralfiekowati@gmail.com>
2018-06-18 20:46:17 +0700

    +Add a file
    Adding a file to test whether there is irrelevant search result for the given search key.

---
+++ src/com/automationpractice/selenium/update/RelevantSearchResultTest.java
@@ -0,0 +1,45 @@
+package com.automationpractice.selenium;
+
+import java.util.ArrayList;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+class RelevantSearchResultTest {
+
+	WebElement element, element2;
+	String productTitle = "";
+	int j;
+
+	public void relevantResult(WebDriver driver) {
+
+		driver.findElement(By.id("search_query_top")).sendKeys("Dress");
+		element = driver.findElement(By.name("submit_search"));
+		element.click();
+
+		ArrayList<WebElement> productResultList = new ArrayList<WebElement>(
+				driver.findElements(By.className("product_img_link")));
+
+		System.out.println("Total number of product result : " + productResultList.size());
+
+		if (productResultList != null && productResultList.size() > 0) {
+
+			for (int i = 0; i < productResultList.size(); i++) {
+
+				j = i + 1;
+				productTitle = productResultList.get(i).getAttribute("title");
+
+				if (productTitle.contains("Dress") == true || productTitle.contains("dress") == true) {
+					System.out.println("Relevant product " + j + ": " + productTitle);
+				} else {
+					System.out.println("Irrelevant product " + j + ": " + productTitle);
+				}
+
+			}
+
+		}
+
+	}
+
+}
