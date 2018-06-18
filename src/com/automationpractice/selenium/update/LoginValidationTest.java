a8b4708184665ea5233381c7d5170f5654174e31
Nur Alfi Ekowati <nuralfiekowati@gmail.com>
2018-06-18 16:58:58 +0700

    -Update file
    Updating file to validation test.

---
+++ src/com/automationpractice/selenium/update/LoginValidationTest.java
@@ -0,0 +1,36 @@
+package com.automationpractice.selenium;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+class LoginValidationTest {
+
+	WebElement element;
+
+	public void validLogin(WebDriver driver) {
+
+		element = driver.findElement(By.className("login"));
+		element.click();
+
+		driver.findElement(By.id("email")).sendKeys("nuralfiekowati@yahoo.com");
+		driver.findElement(By.id("passwd")).sendKeys("lalala");
+		driver.findElement(By.id("SubmitLogin")).click();
+
+		List<WebElement> errorList = driver.findElements(By.id("authentication"));
+
+		System.out.println("Error count: " + errorList.size());
+
+		if (errorList != null && errorList.size() > 0)
+		{
+			System.out.println("Authentication failed!");
+		}
+		else {
+			System.out.println("Successfully login!");
+		}
+
+	}
+
+}
