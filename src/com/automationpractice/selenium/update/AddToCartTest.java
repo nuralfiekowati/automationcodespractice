11010d3d52a089daf98be58dc854890ec98b52c6
Nur Alfi Ekowati <nuralfiekowati@gmail.com>
2018-06-20 00:24:44 +0700

    +Add a file
    Adding a file to test the working of the cart.

---
+++ src/com/automationpractice/selenium/update/AddToCartTest.java
@@ -0,0 +1,33 @@
+package com.automationpractice.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class AddToCartTest {
+
+	WebElement element, element2, element3, element4;
+
+	public void AddToCart(WebDriver driver) {
+
+		driver.findElement(By.id("search_query_top")).sendKeys("Dress");
+		element = driver.findElement(By.name("submit_search"));
+		element.click();
+		element2 = driver.findElement(By.partialLinkText("Printed Dress"));
+		element2.click();
+		element3 = driver.findElement(By.id("add_to_cart"));
+		element3.click();
+
+		WebElement quantityCart = driver.findElement(By.className("icon-ok"));
+		WebDriverWait wait = new WebDriverWait(driver,
+				20 /* timeout in seconds */);
+		if (wait.until(ExpectedConditions.visibilityOf(quantityCart)) != null)
+			System.out.println("Added to the cart successfully!");
+		else
+			System.out.println("Not added to the cart!");
+
+	}
+
+}
