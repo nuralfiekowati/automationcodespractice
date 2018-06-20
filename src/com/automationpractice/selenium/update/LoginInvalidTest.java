488624ca92fa5d53adc571f4855ff735199086b3
Nur Alfi Ekowati <nuralfiekowati@gmail.com>
2018-06-20 16:32:10 +0700

    -Update file
    Updating a file to check login with invalid account.

--- src/com/automationpractice/selenium/update/LoginInvalidTest.java
+++ src/com/automationpractice/selenium/update/LoginInvalidTest.java
@@ -26,5 +26,39 @@ class LoginInvalidTest {
 		}
 
 	}
+	
+	public void invalidLogin2(WebDriver driver) {
+
+		element = driver.findElement(By.className("login"));
+		element.click();
+
+		driver.findElement(By.id("email")).sendKeys("nuralfiekowati@yahoo.com");
+		driver.findElement(By.id("passwd")).sendKeys("");
+		driver.findElement(By.id("SubmitLogin")).click();
+
+		List<WebElement> errorList = driver.findElements(By.className("page-heading"));
+
+		if (errorList != null && errorList.size() > 0) {
+			System.out.println("Error found: Authentication failed!");
+		}
+
+	}
+	
+	public void invalidLogin3(WebDriver driver) {
+
+		element = driver.findElement(By.className("login"));
+		element.click();
+
+		driver.findElement(By.id("email")).sendKeys("");
+		driver.findElement(By.id("passwd")).sendKeys("lalala");
+		driver.findElement(By.id("SubmitLogin")).click();
+
+		List<WebElement> errorList = driver.findElements(By.className("page-heading"));
+
+		if (errorList != null && errorList.size() > 0) {
+			System.out.println("Error found: Authentication failed!");
+		}
+
+	}
 
 }
