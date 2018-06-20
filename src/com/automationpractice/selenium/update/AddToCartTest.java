8228af16e0078d147c0959a3c02107f333e477b1
Nur Alfi Ekowati <nuralfiekowati@gmail.com>
2018-06-20 13:32:45 +0700

    +Add file - Update file
    Adding file to test test button create account. Updating file to test button add to cart.

--- src/com/automationpractice/selenium/update/AddToCartTest.java
+++ src/com/automationpractice/selenium/update/AddToCartTest.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class AddToCartTest {
+class AddToCartTest {
 
 	WebElement element, element2, element3, element4;
 
